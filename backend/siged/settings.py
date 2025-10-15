b9903e8b1d40d891eba466e13b238a05ebaaf0d1
JuanSanguinoRin <juansebastiansari@ufps.edu.co>
2025-10-14 20:26:37 -0500

    Creacion de la estructura del frontend

--- backend/siged/settings.py
+++ backend/siged/settings.py
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'siged.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_euV9ARzmt47HsJm94ZN',  # tu contraseña
+        'HOST': 'pg-67749f1-ufps-fd2a.l.aivencloud.com',
+        'PORT': '13485',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
